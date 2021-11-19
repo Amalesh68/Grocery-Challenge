0b4910f2a87dc73e75f6596b87123d14af44e177
Amalesh68 <amaleshkumar68@gmail.com>
2021-11-19 15:28:39 +0530

    Update gro.js

--- gro.js
+++ gro.js
@@ -59,8 +59,8 @@ function ajax(){//Creating an XHR Object
             // s5[i].style.backgroundColor = "indianred";
             // s6[i].style.backgroundColor = "indianred";
             // s7[i].style.backgroundColor = "indianred";
-            s1[i].style.textDecoration = "line-through";
-            // s2[i].style.textDecoration = "line-through";
+            // s1[i].style.textDecoration = "line-through";
+            s2[i].style.textDecoration = "line-through";
             // s3[i].style.textDecoration = "line-through";
             // s4[i].style.textDecoration = "line-through";
             // s5[i].style.textDecoration = "line-through";
